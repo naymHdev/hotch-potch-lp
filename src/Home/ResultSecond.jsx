36adf3d36ded2ded5513cdc0374e5df35f2a5876
MacBook Pro <naym100m@gmail.com>
2024-05-14 20:28:55 +0600

    result page compleated

--- src/Home/ResultSecond.jsx
+++ src/Home/ResultSecond.jsx
@@ -3,7 +3,7 @@ import Button from "../Hooks/Button";
 const ResultSecond = () => {
   return (
     <>
-      <section className=" mt-20 grid md:flex gap-5 md:gap-40 w-11/12 mx-auto">
+      <section className=" mt-20 grid md:flex gap-5  w-11/12 mx-auto">
         <div className=" flex-1 mt-20">
           <h2 className=" text-3xl font-bold text-slate-50">
             write what you want to convey through clear, & authentic writing.
@@ -17,7 +17,7 @@ const ResultSecond = () => {
             <Button content="Start 14 Days Free Trial" />
           </div>
         </div>
-        <div className="bg-[#FFFFFF] flex-1 rounded-md p-5 grid grid-cols-1 md:grid-cols-5 gap-2">
+        <div className="bg-[#FFFFFF] flex-1 order-first md:order-last rounded-md p-5 grid grid-cols-1 md:grid-cols-5 gap-2">
           <div className=" col-span-full md:col-span-2">
             <div className=" grid space-y-2 font-thin text-slate-600 text-sm">
               <p>Blog Outline</p>
@@ -26,7 +26,7 @@ const ResultSecond = () => {
               <p>AIDA</p>
               <p>PAS</p>
               <p>Content Rewriter</p>
-              <div className=" grid space-y-2">
+              <div className=" grid space-y-7">
                 <div className=" mt-2 border border-b-4 border-[#E1E3E8] rounded-full w-7/12" />
                 <div className=" mt-2 border border-b-4 border-[#E1E3E8] rounded-full w-4/12" />
                 <div className=" mt-2 border border-b-4 border-[#E1E3E8] rounded-full w-6/12" />
@@ -39,7 +39,33 @@ const ResultSecond = () => {
               </div>
             </div>
           </div>
-          <div className=" col-span-full md:col-span-3"></div>
+          <div className=" col-span-full md:col-span-3">
+            <h4 className=" text-xl font-bold">Results</h4>
+            <p className=" text-sm font-medium text-slate-500">
+              Here's what our AI came up with
+            </p>
+            <div className=" rounded-md bg-black p-3 mt-5">
+              <div className=" mt-2 border border-b-4 border-[#E1E3E8] rounded-full w-3/12" />
+              <div className=" mt-5 border border-b-4 border-[#E1E3E8] rounded-full w-10/12" />
+              <div className=" mt-3 border border-b-4 border-[#E1E3E8] rounded-full w-9/12" />
+              <div className=" mt-3 border border-b-4 border-[#E1E3E8] rounded-full w-7/12" />
+            </div>
+            <div className="bg-[#F8FAFF] p-3 mt-5 rounded-md">
+              <div className=" mt-2 border border-b-4 border-[#E1E3E8] rounded-full w-3/12" />
+              <div className=" mt-5 border border-b-4 border-[#E1E3E8] rounded-full w-10/12" />
+              <div className=" mt-3 border border-b-4 border-[#E1E3E8] rounded-full w-9/12" />
+              <div className=" mt-3 border border-b-4 border-[#E1E3E8] rounded-full w-7/12" />
+            </div>
+            <div className="bg-[#F8FAFF] p-3 mt-5 rounded-md">
+              <div className=" mt-2 border border-b-4 border-[#E1E3E8] rounded-full w-3/12" />
+              <div className=" mt-5 border border-b-4 border-[#E1E3E8] rounded-full w-10/12" />
+              <div className=" mt-3 border border-b-4 border-[#E1E3E8] rounded-full w-9/12" />
+              <div className=" mt-3 border border-b-4 border-[#E1E3E8] rounded-full w-7/12" />
+            </div>
+            <div className="bg-[#F8FAFF] pt-3 mt-5 rounded-md">
+              <div className=" mt-2 border border-b-4 border-[#E1E3E8] rounded-full w-3/12" />
+            </div>
+          </div>
         </div>
       </section>
     </>
